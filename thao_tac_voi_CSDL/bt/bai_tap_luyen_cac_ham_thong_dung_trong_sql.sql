8ded6dd4a0b5259dd2c627510253b23ce9426a08
Your Name <nhathuy.155@gmail.com>
2020-07-16 08:54:49 +0700

    update

--- thao_tac_voi_CSDL/bt/bai_tap_luyen_cac_ham_thong_dung_trong_sql.sql
+++ thao_tac_voi_CSDL/bt/bai_tap_luyen_cac_ham_thong_dung_trong_sql.sql
@@ -1,4 +1,23 @@
-use student_manage;
+CREATE DATABASE student_management;
+USE student_management;
+CREATE TABLE student(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20),
+    age INT,
+    course VARCHAR(20),
+	course_fee INT
+    );
+    
+    INSERT INTO student(`name`, age, course, course_fee)
+    VALUES
+    ('Hoang', 21, 'CNTT', 400000),
+    ('Viet', 19, 'DTVT', 320000),
+    ('Huong', 18, 'KTDN', 600000),
+    ('Nhan', 19, 'CK', 450000),
+    ('Huong', 22, 'TCNH', 500000),
+    ('Hong', 20, 'CNSH', 200000);
+    
+    DROP DATABASE student_management;
 
 select name_student,sum(sotien) as tongtien from students
 where name_student="Huong"
