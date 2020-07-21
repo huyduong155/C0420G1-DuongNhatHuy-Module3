0ba1d9a322a0ffe77ef1c7d8722ef8deecd5b907
huyduong155 <nhathuy.155@gmail.com>
2020-07-21 08:15:55 +0700

    update

--- case_study/furama.sql
+++ case_study/furama.sql
@@ -168,7 +168,7 @@ INSERT INTO `furama`.`khach_hang` (`ho_ten`, `ngay_sinh`, `so_CMND`, `SDT`, `ema
 VALUES 
 ('thinh', '1993-12-10', '1234567', '1234567', 'abc@ab.abc', 'da nang', '1'),
 ('vu', '1993-12-10', '1234567', '1234567', 'abc@ab.abc', 'da nang', '2'),
-('tuong', '1993-12-10', '1234567', '1234567', 'abc@ab.abc', 'quang nam', '3'),
+('tuong', '1993-12-10', '1234567', '1234567', 'abc@ab.abc', 'quang nam', '1'),
 ('hoat', '1993-12-10', '1234567', '1234567', 'abc@ab.abc', 'quang tri', '4'),
 ('viet', '1993-12-10', '1234567', '1234567', 'abc@ab.abc', 'quang tri', '5');
 
@@ -182,6 +182,52 @@ VALUES
  ('thanh', '1982-06-10', '123456789', '5000', '12345677889', 'abc@abc.com', 'abc', '1', '1', '3'),
  ('vu', '1999-06-10', '123456789', '5000', '12345677889', 'abc@abc.com', 'abc', '1', '2', '1');
  
+ INSERT INTO `furama`.`loai_dich_vu` (`ten_loai_dich_vu`) 
+ VALUES 
+ ('villa'),
+ ('house'),
+ ('room');
+ 
+ INSERT INTO `furama`.`kieu_thue` (`ten_kieu_thue`, `gia`) 
+VALUES 
+('nam', '120'),
+('thang', '10'),
+('ngay', '3'),
+('gio', '1');
+ 
+ INSERT INTO `furama`.`dich_vu` (`ten_dich_vu`, `dien_tich`, `so_tang`, `so_nguoi_toi_da`, `chi_phi_thue`, `trang_thai`, `id_kieu_thue`, `id_loai_dich_vu`)
+ VALUES 
+ ('villa1', '30', '2', '10', '30', 'avairable', '1', '1'),
+ ('villa2', '30', '1', '5', '15', 'avairable', '1', '1'),
+ ('house1', '30', '2', '10', '20', 'avairable', '2', '2'),
+ ('house2', '30', '1', '5', '10', 'avairable', '2', '2'),
+ ('room1', '30', '2', '10', '15', 'avairable', '3', '3'),
+ ('room2', '30', '1', '5', '8', 'avairable', '3', '3');
+
+INSERT INTO `furama`.`hop_dong` (`ngay_lam_hop_dong`, `ngay_ket_thuc`, `tien_dat_coc`, `tong_tien`, `id_nhan_vien`, `id_khach_hang`, `id_dich_vu`) 
+VALUES 
+('2019-10-19', '2019-10-22', '123', '456', '1', '1', '1'),
+('2019-01-19', '2019-10-19', '123', '456', '2', '1', '2'),
+('2019-03-19', '2019-04-19', '123', '456', '3', '3', '3'),
+('2019-04-19', '2020-04-19', '123', '456', '4', '4', '4');
+
+INSERT INTO `furama`.`dich_vu_di_kem` (`ten_dich_vu_di_kem`, `gia`, `don_vi`, `trang_thai_kha_dung`) 
+VALUES 
+('massage', '15', '2', 'avairable'),
+('karaoke', '15', '3', 'avairable'),
+('thức an', '15', '4', 'avairable'),
+('nước uống', '15', '4', 'avairable'),
+('thuê xe', '15', '5', 'avairable');
+
+INSERT INTO `furama`.`hop_dong_chi_tiet` (`so_luong`, `id_hop_dong`, `id_dich_vu_di_kem`) 
+VALUES 
+('2', '1', '1'),
+('2', '2', '1'),
+('2', '3', '2'),
+('2', '1', '3');
+
+
+
  -- TAST 2: Hiển thị thông tin nhân viên trong hệ thống có tên bắt đầu một trong các kí tự "H","T" hoặc "K"
  
  SELECT *
@@ -201,6 +247,52 @@ HAVING age <= 50 AND age >= 18
 
 -- TAST 4: Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng.
 -- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+
+SELECT k.ho_ten, COUNT(h.id_khach_hang) AS 'số lần đặt phòng'
+FROM khach_hang k
+        JOIN hop_dong h ON k.id_khach_hang = h.id_khach_hang
+WHERE k.id_loai_khach = 1
+GROUP BY k.ho_ten
+ORDER BY COUNT(h.id_khach_hang);
+
+-- TAST 5: Hiển thị IDKhachHang, HoTen, TenLoaiKhach, IDHopDong, TenDichVu, NgayLamHopDong, NgayKetThuc, 
+-- TongTien (Với TongTien được tính theo công thức như sau: ChiPhiThue + SoLuong*Gia, với SoLuong và Giá là từ bảng DichVuDiKem) cho tất cả các Khách hàng đã từng đặt phỏng.
+-- (Những Khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+
+SELECT k.id_khach_hang, k.ho_ten, l.ten_loai_khach, h.id_hop_dong, h.ngay_lam_hop_dong, h.ngay_ket_thuc, d.ten_dich_vu,
+       (d.chi_phi_thue + ld.don_vi * ld.gia) AS 'tổng tiền'
+FROM khach_hang k
+        JOIN loai_khach l ON k.id_loai_khach = l.id_loai_khach
+        LEFT JOIN hop_dong h ON k.id_khach_hang = h.id_khach_hang
+        JOIN dich_vu d ON k.id_khach_hang = d.id_dich_vu
+        LEFT JOIN hop_dong_chi_tiet hc ON h.id_hop_dong = hc.id_hop_dong
+        LEFT JOIN dich_vu_di_kem ld ON hc.id_dich_vu_di_kem = ld.id_dich_vu_di_kem;
+
+-- TAST 6: Hiển thị IDDichVu, TenDichVu, DienTich, ChiPhiThue, TenLoaiDichVu của tất cả các loại Dịch vụ chưa từng được Khách hàng 
+-- thực hiện đặt từ quý 1 của năm 2019 (Quý 1 là tháng 1, 2, 3).
+
+select d.id_dich_vu, d.ten_dich_vu, d.dien_tich, d.chi_phi_thue, l.ten_loai_dich_vu
+from dich_vu d
+join loai_dich_vu l on d.id_loai_dich_vu = l.id_loai_dich_vu
+where d.id_dich_vu not in (
+select d.id_dich_vu
+from dich_vu d
+join hop_dong h on d.id_dich_vu = h.id_dich_vu
+where (month(h.ngay_lam_hop_dong) = 1 or month(h.ngay_lam_hop_dong) = 2 or month(h.ngay_lam_hop_dong) = 3) 
+and year(h.ngay_lam_hop_dong) = 2019
+);
+
+-- TAST 7: Hiển thị thông tin IDDichVu, TenDichVu, DienTich, SoNguoiToiDa, ChiPhiThue, TenLoaiDichVu
+-- của tất cả các loại dịch vụ đã từng được Khách hàng đặt phòng trong năm 2018 nhưng chưa từng được Khách hàng đặt phòng  trong năm 2019.
+
+-- select d.id_dich_vu, d.ten_dich_vu, d.dien_tich, d.so_nguoi_toi_da, d.chi_phi_thue, ld.ten_loai_dich_vu
+-- from dich_vu d
+-- join loai_dich_vu ld on d.id_loai_dich_vu = ld.id_loai_dich_vu
+-- where d.id_dich_vu not in (
+-- select d.id_dich_vu
+-- from dich_vu d
+-- join hop_dong h on d.id_dich_vu = h.id_dich_vu
+-- where year(h.ngay_lam_hop_dong) = 2019);
 
 
 
